92b857d3e42ac4c6d6fb95051c446fdee25d6567
Sujal Lama <sujallama@Sujals-MacBook-Air.local>
2023-07-21 11:36:20 +0545

    webpack and babel project setup for react

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/* The configuration in this file tells Webpack to use babel-loader for every file
+    that has the .js extension and excludes files in the node_modules directory
+    for the Babel compiler.
+*/
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        })
+    ]
+}
